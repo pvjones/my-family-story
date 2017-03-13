9b30691443d0fcac27ddd7972eca10793062945a
Paul Jones <paulvjones@gmail.com>
2017-03-13 15:21:19 -0600

    homepage changes, gulpfile update

--- public/app/components/home/home.controller.js
+++ public/app/components/home/home.controller.js
@@ -6,7 +6,13 @@
 
   function homeController($scope) {
 
+    // $(window).scroll((e) => {
+    //   let winScroll = $(window).scrollY();
 
+    //   $('.bg')
+    //     .css('transform',`translateY(+${winScroll / 15}%)`);
+
+    // })
 
   };
 })();
