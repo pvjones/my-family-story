f9d3fc340f51655d1cc30f7c12d9a746124d2565
Paul Jones <paulvjones@gmail.com>
2017-03-14 13:26:51 -0600

    fixed link styling on homepage and nav

--- public/app/components/home/home.controller.js
+++ public/app/components/home/home.controller.js
@@ -5,11 +5,11 @@
     .controller('homeController', homeController);
 
   function homeController($scope) {
-
+    console.log('homecontroller')
     $scope.howItWorks = [
       {
         imgUrl: '../../../assets/images/HIW1.png',
-        desc: 'Select the number & type of pages (<a class="blue-link" ui-sref="start">Learn more about pages</a>)'
+        desc: 'Select the number & type of pages - <a class="blue-link" ui-sref="start">learn more about pages</a>'
       },
       {
         imgUrl: '../../../assets/images/HIW2.png',
