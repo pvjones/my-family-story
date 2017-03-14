f053dee83763b604be458ad37433463d8dd19caa
juliecoding <julie.akohler@gmail.com>
2017-03-14 17:24:34 -0600

    Added missing CRUD methods to products ctrl

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -2,8 +2,29 @@ var Product = require('../models/product.model.js');
 
 module.exports = {
 
+  createProduct: function(req, res, next) {
+    Product.create(req.body, function(err, product) {
+      if (err) return res.status(500).send(err);
+      res.status(200).send(product);
+    });
+  },
+
   readProduct: function(req, res, next) {
     Product.find(req.query, function(err, product) {
+      if (err) return res.status(500).send(err);
+      res.status(200).send(product);
+    });
+  }
+
+  updateProduct: function(req, res, next) {
+    Product.findByIdAndUpdate(req.params.id, req.body, {new: true}, function(err, product) {
+      if (err) return res.status(500).send(err);
+      res.status(200).send(product);
+    });
+  },
+
+  deleteProduct: function(req, res, next) {
+    Product.findByIdAndRemove(req.params.id, function(err, product) {
       if (err) return res.status(500).send(err);
       res.status(200).send(product);
     });
