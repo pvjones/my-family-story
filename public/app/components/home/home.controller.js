c9583b58ada98af9f6c47fd4d8695406c79c061d
Paul Jones <paulvjones@gmail.com>
2017-03-18 11:48:44 -0600

    style and content adjustments

--- public/app/components/home/home.controller.js
+++ public/app/components/home/home.controller.js
@@ -9,15 +9,15 @@
     $scope.howItWorks = [
       {
         imgUrl: '../../../assets/images/HIW1.png',
-        desc: 'Customize your coloring book and tell your family\'s story. Select the number and type of pages and submit your text.<br /><a class="underline-link" href="/#!/start"><em>Learn more</em></a>'
+        desc: 'Customize your coloring book and tell your family\'s story. Select the number and type of pages and submit your text.<br /><a class="underline-link about-link" href="/#!/start"><em>Learn more</em></a>'
       },
       {
         imgUrl: '../../../assets/images/HIW2.png',
-        desc: 'We\'ll draw the perfect pictures to correspond with your text and preferences for each page.<br /><a class="underline-link" href="/#!/start"><em>Learn more</em></a>'
+        desc: 'We\'ll draw the perfect pictures to correspond with your text and preferences for each page.<br /><a class="underline-link about-link" href="/#!/start"><em>Learn more</em></a>'
       },
       {
         imgUrl: '../../../assets/images/HIW3.png',
-        desc: 'You\'ll recieve your custom coloring book as a digital file, or choose to have it printed and delivered to your door.<br /><a class="underline-link" href="/#!/start"><em>Learn more</em></a>'
+        desc: 'You\'ll recieve your custom coloring book as a digital file, or choose to have it printed and delivered to your door.<br /><a class="underline-link about-link" href="/#!/start"><em>Learn more</em></a>'
       }
     ];
 
