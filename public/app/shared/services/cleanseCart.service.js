94d998c6d266392c405cdeef33aabcfd71e710df
juliecoding <julie.akohler@gmail.com>
2017-03-19 18:58:53 -0600

    Some orders updated, endpoints checked

--- public/app/shared/services/cleanseCart.service.js
+++ public/app/shared/services/cleanseCart.service.js
@@ -1,41 +1,41 @@
-(function() {
-
-  angular
-    .module('app')
-    .service('CleanseCartService', CleanseCartService);
-
-  function CleanseCartService() {
-    this.cleanseOrder = (rawResponse) => {
-      console.log(response);
-      $scope.orderDetails = response.data;
-      $scope.pageCounts = [];
-
-      for (let i = 0; i < $scope.orderDetails.books.length; i++) {
-        let pages = {
-          basic: 0,
-          activity: 0,
-          portrait: 0
-        };
-
-        for (let j = 0; j < $scope.orderDetails.books[i].pages.length; j++) {
-          let type = $scope.orderDetails.books[i].pages[j].page_type;
-          if (type === "Basic") {
-            pages.basic += 1;
-          }
-          else if (type === "Activity") {
-            pages.activity += 1;
-          }
-          else if (type === "Portrait") {
-            pages.portrait += 1;
-          }
-        }
-          $scope.pageCounts.push(pages);
-          $scope.orderDetails.books[i].pageCount = pages;
-        }
-      })
-    }
-
-
-
-  };
-})();
+// (function() {
+//
+//   angular
+//     .module('app')
+//     .service('CleanseCartService', CleanseCartService);
+//
+//   function CleanseCartService() {
+//     this.cleanseOrder = (rawResponse) => {
+//       console.log(response);
+//       $scope.orderDetails = response.data;
+//       $scope.pageCounts = [];
+//
+//       for (let i = 0; i < $scope.orderDetails.books.length; i++) {
+//         let pages = {
+//           basic: 0,
+//           activity: 0,
+//           portrait: 0
+//         };
+//
+//         for (let j = 0; j < $scope.orderDetails.books[i].pages.length; j++) {
+//           let type = $scope.orderDetails.books[i].pages[j].page_type;
+//           if (type === "Basic") {
+//             pages.basic += 1;
+//           }
+//           else if (type === "Activity") {
+//             pages.activity += 1;
+//           }
+//           else if (type === "Portrait") {
+//             pages.portrait += 1;
+//           }
+//         }
+//           $scope.pageCounts.push(pages);
+//           $scope.orderDetails.books[i].pageCount = pages;
+//         }
+//       })
+//     }
+//
+//
+//
+//   };
+// })();
--- server/services/order.service.js
+++ server/services/order.service.js
@@ -1,13 +1,11 @@
-const Book = require('../models/book.model.js');
 const Order = require('../models/order.model.js');
-
 
 module.exports = {
   getOrderDetails: (req, res, next) => {
     Order
       .findById(req.params.id)
-      .populate('books')
-      .exec((err, order) => {
+      .populate("books")
+      .exec(function(err, order) {
         if (err) {
           return res.status(500).send(err);
         }
@@ -15,18 +13,3 @@ module.exports = {
     })
   }
 };
-
-// module.exports = {
-//   getOrderDetails: (req, res, next) => {
-//     Order
-//       .findById(req.params.id)
-//       .populate('Order.books')
-//       .exec((err, order) => {
-//         if (err) {
-//           return res.status(500).send(err);
-//         }
-//         console.log(order);
-//         return res.status(200).send(order);
-//     })
-//   }
-// };
