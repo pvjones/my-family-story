1e893020f1db8b2b223ab80e4f00278e492c3fc4
Paul Jones <paulvjones@gmail.com>
2017-03-24 12:12:56 -0600

    adding states for admin

---
+++ public/app/components/admin/products/adminProducts.controller.js
@@ -0,0 +1,12 @@
+(function () {
+
+  angular
+    .module('app')
+    .controller('adminProductsController', [adminProductsController]);
+
+  function adminProductsController() {
+
+    
+
+  };
+})();
