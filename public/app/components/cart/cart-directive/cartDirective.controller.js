e5f7e11b9e0d056393ed4a484e30a9cacf82825f
Paul Jones <paulvjones@gmail.com>
2017-03-23 23:05:07 -0600

    squashing big fat bugs into littler bugs.

--- public/app/components/cart/cart-directive/cartDirective.controller.js
+++ public/app/components/cart/cart-directive/cartDirective.controller.js
@@ -9,16 +9,17 @@
     let ctrl = this;
 
     $scope.$watch('ctrl.order', (newVal) => {
+      "fired"
       if (newVal) ctrl.cartTotal = getCartTotal(newVal["cart"]);
-    })
+    });
+    // UNNECESSARY?
 
     ctrl.deleteBook = (bookId) => {
       ctrl.order.cart = ctrl.order.cart.filter((elem) => {
         return elem._id !== bookId;
       });
       ctrl.cartTotal = getCartTotal(ctrl.order.cart);
-      $scope.$apply()
-
+      
       CartService.updateCart(ctrl.order)
         .then((res) => {
           ctrl.order = res;
@@ -27,16 +28,20 @@
           ctrl.order = {};
           console.log(err);
         })
-    }
+    };
 
     function getCartTotal(cart) {
       let total = 0;
       cart.forEach((book) => {
-        if (book.print_qty) total += book.print_qty.price;
+        if (book.print_qty) {
+          console.log(typeof(book.print_qty.price), book.print_qty.price)
+          total += book.print_qty.price;
+        }
         book.pageProducts.forEach((pageProduct) => {
           total += pageProduct.subtotal;
         });
       });
+      console.log("cart-total", total)
       return total;
     };
 
@@ -56,7 +61,7 @@
           return ctrl.order._id;
         }
       }
-    })
+    });
       modalInstance.result.then((param) => {
         if (param == 'success') {
           console.log("From cartDirectiveController: Successful payment");
@@ -64,7 +69,7 @@
         if (param == 'cancel') {
           console.log("Cancelled");
         }
-      })
+      });
     };
 
   };
