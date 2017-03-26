628831c31a0d29847dd461da23cb4e94d0079da2
Paul Jones <paulvjones@gmail.com>
2017-03-26 13:47:25 -0600

    add pages to detail view, add framework for archive view

--- public/app/components/admin/archive/adminArchive.controller.js
+++ public/app/components/admin/archive/adminArchive.controller.js
@@ -2,11 +2,23 @@
 
   angular
     .module('app')
-    .controller('adminArchiveController', [adminArchiveController]);
+    .controller('adminArchiveController', ['$scope', 'AdminService', adminArchiveController]);
 
-  function adminArchiveController() {
+  function adminArchiveController($scope, AdminService) {
 
-    
+    getAllActiveOrders();
+
+    function getAllActiveOrders() {
+      AdminService.getAllActiveOrders()
+        .then((res) => {
+          $scope.allOrders = res;
+          console.log(res)
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+
+    };
 
   };
 })();
